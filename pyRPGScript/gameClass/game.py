6095c2a6760f6caa38d921592b3c494b21366a3b
0wlc0de <lyndonnpamintuan@outlook.com>
2018-04-21 20:37:06 +0800

    RPG Script Battle Simulator. Updated Spell Classes and Healing Function for our Player with Items in Inventory Class and HP bars for some graphical view in console.

--- pyRPGScript/gameClass/game.py
+++ pyRPGScript/gameClass/game.py
@@ -1,5 +1,7 @@
 import random
 
+import re
+
 
 class bColors:
     HEADER = '\033[95m'
@@ -13,7 +15,9 @@ class bColors:
 
 
 class Person:
-    def __init__(self, name, hp, mp, atk, df, magic):
+    def __init__(self, name, hp, mp, atk, df, magic, items, player_type):
+        self.player_type = player_type
+        self.items = items
         self.playerName = name
         self.maxHp = hp
         self.hp = hp
@@ -23,7 +27,7 @@ class Person:
         self.atkMax = atk + 10
         self.df = df
         self.magic = magic
-        self.actions = ['Attack', 'Magic']
+        self.actions = ['Attack', 'Magic', 'Items']
 
     def get_name(self):
         return self.playerName
@@ -50,11 +54,19 @@ class Person:
         print("Choose Magic to Attack: ")
         for spells in self.magic:
             if spells.damage > 0:
-                print(iterate, ":", bColors.BOLD + bColors.OKGREEN + spells.name + bColors.ENDC, "dmg:", bColors.BOLD +
-                      bColors.FAIL + str(spells.damage) + bColors.ENDC)
+                print("    ", iterate, ":", bColors.BOLD + bColors.OKGREEN + spells.name + bColors.ENDC, "dmg:",
+                      bColors.BOLD + bColors.FAIL + str(spells.damage) + bColors.ENDC)
             else:
-                print(iterate, ":", bColors.BOLD + bColors.OKGREEN + spells.name + bColors.ENDC, "heal:", bColors.BOLD +
-                      bColors.FAIL + str(spells.healing) + bColors.ENDC)
+                print("    ", iterate, ":", bColors.BOLD + bColors.OKGREEN + spells.name + bColors.ENDC, "heal:",
+                      bColors.BOLD + bColors.FAIL + str(spells.healing) + bColors.ENDC)
+            iterate += 1
+
+    def get_items(self):
+        iterate = 1
+        print("Choose Items in your Inventory")
+        for item in self.items:
+            print("    ", iterate, ":", bColors.OKBLUE + item.name + bColors.ENDC, ">",
+                  bColors.OKGREEN + "\"" + item.description + "\"" + bColors.ENDC, "x" + str(item.qty))
             iterate += 1
 
     def get_magic_class(self, i):
@@ -69,3 +81,38 @@ class Person:
         self.hp += healing
         if self.hp > self.maxHp:
             self.hp = self.maxHp
+
+    def take_mp_healing(self, healing):
+        self.mp += healing
+        if self.mp > self.maxMp:
+            self.mp = self.maxMp
+
+    def get_stats(self):
+        hp_bar = ""
+        bar_ticks = (self.hp / self.maxHp) * 100 / 4
+
+        mp_bar = ""
+        mp_bar_ticks = (self.mp / self.maxMp) * 100 / 10
+
+        while bar_ticks > 0:
+            hp_bar += "█"
+            bar_ticks -= 1
+
+        while len(hp_bar) < 25:
+            hp_bar += "  "
+
+        while mp_bar_ticks > 0:
+            mp_bar += "█"
+            mp_bar_ticks -= 1
+
+        while len(mp_bar) < 10:
+            mp_bar += " "
+
+        spaces = re.sub('[a-zA-Z0-9]', ' ', self.playerName)
+        print("Name", "(", self.player_type, ")")
+        print(spaces + "           ________________________________________")
+        print(self.playerName + ":" + "   HP :  |" + bColors.OKGREEN + bColors.BOLD + hp_bar + bColors.ENDC)
+        print(spaces + "           ________________")
+        print(spaces + " " + "   MP :  |" + bColors.OKBLUE + bColors.BOLD + mp_bar + bColors.ENDC)
+        print("\n")
+
--- pyRPGScript/main.py
+++ pyRPGScript/main.py
@@ -1,41 +1,66 @@
 import random
 from pyRPGScript.gameClass.game import Person, bColors
 from pyRPGScript.gameClass.Spells import Spell
+from pyRPGScript.gameClass.inventory import Items
 
 
+# Create Magic Attacks
 ThunderBolt = Spell("ThunderBolt", 100, 120, 0, "Thunder")
 HyperBeam = Spell("Hyper Beam", 120, 160, 0, "Normal")
 FireBlast = Spell("Fire Blast", 100, 120, 0, "Fire")
 Synthesis = Spell("Synthesis", 40, 0, "Normal", 200)
 arrayAttacks = [ThunderBolt, HyperBeam, FireBlast, Synthesis]
 
+# Create Items in Inventory
+Potion = Items("Potion", "HP", "Heals 100 of your Hit Points", 100, 5)
+Elixir = Items("Elixir", "Restoration", "Heals 100 of your HP and MP", 100, 5)
+Grenade = Items("Grenade", "Damage", "Deals 50 HP to your Opponents", 50, 2)
+arrayItems = [Potion, Elixir, Grenade]
+
+# Create Player Variables
 playerName = input("Enter Player Name: ")
 EnemyName = ["Harry", "Potter", "Carlos"]
+Player1 = Person(playerName, 500, 250, 100, 10, arrayAttacks, arrayItems, "Player")
+Player2 = Person(EnemyName[random.randrange(0, len(EnemyName))], 500, 100, 150, 10,
+                 [ThunderBolt, HyperBeam, FireBlast], arrayItems, "Opponent")
 
-Player1 = Person(playerName, 500, 250, 100, 10, arrayAttacks)
-Player2 = Person(EnemyName[random.randrange(0, len(EnemyName))], 500, 100, 150, 10, [ThunderBolt, HyperBeam, FireBlast])
 
-
+# Create Damage Message for Simplicity Passing of Objects
 def damage_message(Attacker, Defender, generated_damage, attack_name):
-    print(bColors.OKBLUE + Attacker.get_name() + bColors.ENDC, "uses",
+    print(bColors.OKGREEN + bColors.BOLD + "Battle Status: " + bColors.ENDC,
+          bColors.OKBLUE + Attacker.get_name() + bColors.ENDC, "uses",
           bColors.OKGREEN + attack_name + bColors.ENDC, "on",
           bColors.WARNING + Defender.get_name() + bColors.ENDC, "!!! damage output:", generated_damage,
           bColors.WARNING + Defender.get_name() + bColors.ENDC, "remaining HP: ",
           bColors.FAIL + str(Defender.hp) + bColors.ENDC, "/", bColors.FAIL + str(Defender.maxHp) + bColors.ENDC)
 
 
-def healing_message(Player, attack_name, healing):
+# Create Healing Message for Simplicity Passing of Objects
+def healing_message(Player, attack_name, healing, type_heal):
+    content = 'HP'
+    if type_heal == 'Restoration':
+        content = 'HP and MP'
+
     print(bColors.OKBLUE + Player.get_name() + bColors.ENDC, "uses",
           bColors.OKGREEN + attack_name + bColors.ENDC, "to Heal",
-          bColors.OKGREEN + str(healing) + bColors.ENDC, "of his health.",
+          bColors.OKGREEN + str(healing) + bColors.ENDC, "of his " + content + ".",
           "remaining HP: ", bColors.FAIL + str(Player.hp) + bColors.ENDC, "/",
-          bColors.FAIL + str(Player.maxHp) + bColors.ENDC)
+          bColors.FAIL + str(Player.maxHp) + bColors.ENDC,
+          "remaining MP: ", bColors.OKBLUE + str(Player.mp) + bColors.ENDC, "/",
+          bColors.OKBLUE + str(Player.maxMp) + bColors.ENDC)
 
 
+print("\n")
+
+Player1.get_stats()
+
+# Initial Printing of Enemy Name and Starting the game using the runningGame boolean
 print("Your Enemy is:", bColors.WARNING + Player2.get_name() + bColors.ENDC, "HP:",
       bColors.FAIL + str(Player2.hp) + bColors.ENDC, "/", bColors.FAIL + str(Player2.maxHp) + bColors.ENDC)
 runningGame = True
 
+
+# The whole game in While Loop
 while runningGame:
     print("=============================================")
     Player1.generate_actions()
@@ -46,7 +71,7 @@ while runningGame:
         damage_message(Player1, Player2, generatedDamage, "Normal Attack")
     elif chooseAction == '2':
         Player1.get_magic()
-        print(str(len(arrayAttacks) + 1), ": Back To Actions")
+        print("    ", str(len(arrayAttacks) + 1), ": Back To Actions")
         chooseMagic = int(input("Choose Magic Attack: ")) - 1
 
         if chooseMagic == (len(arrayAttacks)):
@@ -65,9 +90,36 @@ while runningGame:
         elif SpellClass.healing > 0:
             generateHealing = SpellClass.healing
             Player1.take_healing(generateHealing)
-            healing_message(Player1, SpellClass.get_magic_name(), SpellClass.healing)
+            healing_message(Player1, SpellClass.get_magic_name(), SpellClass.healing, SpellClass.type)
 
         Player1.take_manacost(SpellClass.get_magic_cost())
+    elif chooseAction == '3':
+        Player1.get_items()
+        print("    ", str(len(arrayItems) + 1), ": Back To Actions")
+        chooseItem = int(input("Choose Item: ")) - 1
+
+        if chooseItem == (len(arrayItems)):
+            continue
+
+        ItemClass = Player1.items[chooseItem]
+
+        if ItemClass.type == 'HP':
+            Player1.take_healing(ItemClass.deal)
+            healing_message(Player1, ItemClass.name, ItemClass.deal, "Normal")
+            ItemClass.take_qty(1)
+        elif ItemClass.type == 'Restoration':
+            Player1.take_healing(ItemClass.deal)
+            Player1.take_mp_healing(ItemClass.deal)
+            healing_message(Player1, ItemClass.name, ItemClass.deal, ItemClass.type)
+            ItemClass.take_qty(1)
+        elif ItemClass.type == 'Damage':
+            Player2.take_damage(ItemClass.deal)
+            damage_message(Player1, Player2, ItemClass.deal, ItemClass.name)
+            ItemClass.take_qty(1)
+
+        if ItemClass.qty == 0:
+            Player1.items.remove(ItemClass)
+
     else:
         print(Player1.get_name(), ": ", bColors.BOLD + bColors.FAIL + "Invalid Choice of Action" + bColors.ENDC)
         continue
@@ -75,12 +127,19 @@ while runningGame:
     if Player2.hp == 0:
         print(bColors.OKBLUE + bColors.BOLD + "You Win! Congratulations! " + bColors.ENDC)
         runningGame = False
-        break
-
-    generatedDamage = Player2.generate_damage()
-    Player1.take_damage(generatedDamage)
-    damage_message(Player2, Player1, generatedDamage, "Normal Attack")
+        pass
+    else:
+        generatedDamage = Player2.generate_damage()
+        Player1.take_damage(generatedDamage)
+        damage_message(Player2, Player1, generatedDamage, "Normal Attack")
 
     if Player1.hp == 0:
         print(bColors.FAIL + bColors.BOLD + "You Lost to " + Player2.get_name() + ". Please Try Again." + bColors.ENDC)
         runningGame = False
+        pass
+
+    print("\n")
+    Player1.get_stats()
+    Player2.get_stats()
+
+
