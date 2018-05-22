5277738206f267d0322ed83dd70a7bee52abde17
0wlc0de <lyndonnpamintuan@outlook.com>
2018-05-22 21:13:25 +0800

    This is the Heap Sort Algorithm based on the lecture in MIT OpenCourseWare

--- pyHeap/HeapSort.py
+++ pyHeap/HeapSort.py
@@ -1,28 +1,68 @@
-from math import floor
+"""
+    Heap Sort using the Pseudo-code from MIT OpenCourseWare
+    Complexity of This Algorithm O(n logN)
+"""
 
 
-x_array = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1]
+class HeapSort:
+
+    # defining an initialization
+    # arguments -> Unordered Array
+    def __init__(self, A):
+        self.A = A
+        self.heap_size = len(self.A)
+
+    # Heapify the violated children that are not max heaps
+    def Max_Heapify(self, i, heap_size):
+        l = 2 * i + 1
+        r = 2 * i + 2
+        if l < heap_size and self.A[l] > self.A[i]:
+            largest = l
+        else:
+            largest = i
+        if r <= heap_size - 1 and self.A[r] > self.A[largest]:
+            largest = r
+        if largest != i:
+            swapValue = self.A[i]
+            self.A[i] = self.A[largest]
+            self.A[largest] = swapValue
+            self.Max_Heapify(largest, self.heap_size)
+
+    # Building Max heap in the first step
+    def Build_Max_Heap(self):
+        for i in range(len(self.A), -1, -1):
+            self.Max_Heapify(i, self.heap_size)
+            # print("Sorting", self.A)
+        print("MaX_HEAP : ", self.A)
+
+    # Sort the Array Using the Build_Max_heap to have a Max Heap first, Then swap the first element which is A[0]
+    # to the last element of the Array  ->  A[len(array)-1] then reduce the heap size so that the last element which is
+    # the highest element in the Array will never be move again
+    def sort(self):
+
+        # Step 1: Build Max Heap from Unordered Array
+        self.Build_Max_Heap()
+
+        # Loop the heap size
+        while self.heap_size > 0:
+
+            # Store the last element in a temporary variable
+            swap = self.A[self.heap_size - 1]
+
+            # Swap the value of the root Array -> A[0] to the last element -> A[len(A) - 1]
+            self.A[self.heap_size - 1] = self.A[0]
+            self.A[0] = swap
+
+            # Reduce the Heap size so that the last element will be unaffected with any changes
+            self.heap_size -= 1
+
+            # Call the Max Heapify at 0 index which is the root because it is likely that after swapping the root and
+            # the last element will violate the Max_heap
+            self.Max_Heapify(0, self.heap_size)
+
+        print("Sorted Array : ", self.A)
 
 
-# Complexity O(n)
-# this method is used to create and validate the leftChilds, rightChilds of the Parent Nodes
-# all arrays can be visualize as nearly complete binary tree or HEAP
-def build_heap(array):
-    mid = int(floor(len(array)/2))
-
-    for x in range(mid, -1, -1):
-        print(x, ":", array[x])
-        leftChild_index = 2 * x + 1
-        rightChild_index = 2 * x + 2
-        if leftChild_index > len(array)-1:
-            print(array[x], "has no children")
-        elif leftChild_index == len(array)-1:
-            print("Array PARENT: ", array[x], "CHILDREN: leftChild -", array[leftChild_index], ": rightChild -",
-                  "NONE")
-        elif leftChild_index <= len(array) and rightChild_index < len(array):
-            print("Array PARENT: ", array[x], "CHILDREN: leftChild -", array[leftChild_index], ": rightChild -",
-                  array[rightChild_index])
-
-
-
-build_heap(x_array)
+x_array = [11, 23, 42, 45, 6, 12, 7, 100]
+heapSort = HeapSort(x_array)
+heapSort.sort()
